b94b5ad092449e7b2c3207ec1900c70cafb2d1b9
David Yack <dyack@coloradotc.com>
2018-08-02 23:02:00 -0500

    merged in improved null ref handling in ensuresuccess

--- dotnet/Xrm.Tools.WebAPI/CRMWebAPI.cs
+++ dotnet/Xrm.Tools.WebAPI/CRMWebAPI.cs
@@ -917,48 +917,59 @@ namespace Xrm.Tools.WebAPI
             if (response.IsSuccessStatusCode)
                 return;
 
-            string message = String.Empty;            
-
-            string errorData = response.Content.ReadAsStringAsync().Result;
-
-            if (response.Content.Headers.ContentType.MediaType.Equals("text/plain"))
-            {
-                message = errorData;
-            }
-            else if (response.Content.Headers.ContentType.MediaType.Equals("application/json"))
-            {
-                JObject jcontent = (JObject)JsonConvert.DeserializeObject(errorData);
-                IDictionary<string, JToken> d = jcontent;
-                
-                if (d.ContainsKey("error"))
-                {
-                    JObject error = (JObject)jcontent.Property("error").Value;
-                    message = (String)error.Property("message").Value;
-                }
-                else if (d.ContainsKey("Message"))
-                    message = (String)jcontent.Property("Message").Value;
-
-
-            }
-            else if (response.Content.Headers.ContentType.MediaType.Equals("text/html"))
-            {
-                message = "HTML Error Content:";
-                message += "\n\n" + errorData;
-            }
-            else
-            {
-                message = String.Format("Error occurred and no handler is available for content in the {0} format.",
-                    response.Content.Headers.ContentType.MediaType.ToString());
-            }
+            string message = GetErrorMessageText(response);
 
             var exception = new Xrm.Tools.WebAPI.Results.CRMWebAPIException(message);
 
             if (jsonData != null)
                 exception.JSON = jsonData;
-            
+
             throw exception;
 
         }
+        /// <summary>
+        /// Helper method to extract error message text
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        private static string GetErrorMessageText(HttpResponseMessage response)
+        {
+            string errorData = response.Content.ReadAsStringAsync().Result;
+            string mediaType = response.Content?.Headers?.ContentType?.MediaType;
+
+            if (string.IsNullOrWhiteSpace(errorData) ||
+                string.IsNullOrWhiteSpace(mediaType) ||
+                mediaType.Equals("text/plain"))
+            {
+                return errorData;
+            }
+
+            if (mediaType.Equals("application/json"))
+            {
+                JObject jcontent = (JObject)JsonConvert.DeserializeObject(errorData);
+                IDictionary<string, JToken> d = jcontent;
+
+                if (d.ContainsKey("error"))
+                {
+                    JObject error = (JObject)jcontent.Property("error").Value;
+                    return (String)error.Property("message")?.Value ?? errorData;
+                }
+
+                if (d.ContainsKey("Message"))
+                    return (String)jcontent.Property("Message").Value;
+            }
+            else if (mediaType.Equals("text/html"))
+            {
+                return "HTML Error Content:\n\n" + errorData;
+            }
+            else
+            {
+                return $"Error occurred and no handler is available for content in the {response.Content.Headers.ContentType.MediaType} format.";
+            }
+
+            return errorData;
+        }
+
 
         /// <summary>
         /// Helper method to relace the '_x002e_', '_x0040_' and '_TEXT_value' for the '.', '@' and 'TEXT'
